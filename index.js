70fc8f31f2559e16db97250752792570b935947d
krudoppler <bkru33@gmail.com>
2025-02-10 18:14:43 -0500

    added deck_id

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-// HTML Elements
+// Global Variable
+let deckId = "";
 
+// HTML Elements
 
 // Event Listeners
 document.getElementById("get-api-button").addEventListener("click", getCardApi);
@@ -8,19 +10,9 @@ function getCardApi() {
   fetch("https://deckofcardsapi.com/api/deck/new/shuffle/")
     .then((r) => r.json())
     .then((data) => {
-      console.log(data);
+      deckId = data.deck_id;
+      console.log(deckId);
     });
 }
 
 
-const voters = [
-  {name: "Joe", email: "joe@joe.com", voted: true},
-  {name: "Jane", email: "jane@jane.com", voted: true},
-  {name: "Bo", email: "bo@bo.com", voted: false},
-  {name: "Bane", email: "bane@bane.com", voted: false}
-]
-
-const filteredVoters = voters.filter(voter => voter.voted).map(voter => voter.email)
-
-
-console.log(filteredVoters)

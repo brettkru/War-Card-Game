d302402f6b173fb23c3aa30d0a3475fded29c0ca
krudoppler <bkru33@gmail.com>
2025-02-14 11:31:07 -0500

    commit

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ function getCardApi() {
     .then((r) => r.json())
     .then((data) => {
       deckId = data.deck_id;
-      console.log(deckId);
     });
 }
 
@@ -23,9 +22,32 @@ function drawCards() {
     .then((data) => {
       let renderedHtml = "";
       let cards = data.cards;
-      cards.forEach((card) => {
+      
+      function getCardValue(card) {
+        if (card.value === "ACE" || card.value === "JACK" || card.value === "QUEEN" || card.value === "KING") {
+          return 10
+        } else {
+          return parseInt(card.value)
+        }
+      }
+
+      let card1 = getCardValue(cards[0])
+      let card2 = getCardValue(cards[1])
+      cardComparison(card1, card2);
+
+      cards.forEach((card, index) => {
         renderedHtml += `<img src="${card.image}" alt="Card Image">`;
       });
       document.getElementById("card-images").innerHTML = renderedHtml;
     });
+}
+
+function cardComparison(card1, card2) {
+  if (card1 > card2){
+    console.log("Card 1 Wins!")
+  } else if (card1 == card2) {
+    console.log("Its a Tie!")
+  } else {
+    console.log("Card 2 Wins!")
+  }
 }

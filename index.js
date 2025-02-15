6c53ea9515c6765600ed47005fbb86ff38308701
krudoppler <bkru33@gmail.com>
2025-02-14 20:13:50 -0500

    added more cardy poos

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ function getCardApi() {
   fetch("https://deckofcardsapi.com/api/deck/new/shuffle/")
     .then((r) => r.json())
     .then((data) => {
+      
       deckId = data.deck_id;
+      
     });
 }
 
@@ -20,34 +22,46 @@ function drawCards() {
   fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
     .then((r) => r.json())
     .then((data) => {
+      
       let renderedHtml = "";
       let cards = data.cards;
+      let cardsRemaming = data.remaining
+      console.log(cardsRemaming)
       
+
       function getCardValue(card) {
-        if (card.value === "ACE" || card.value === "JACK" || card.value === "QUEEN" || card.value === "KING") {
-          return 10
+        if (
+          card.value === "JACK" ||
+          card.value === "QUEEN" ||
+          card.value === "KING"
+        ) {
+          return 10;
+        } else if (card.value === "ACE") {
+          return 11;
         } else {
-          return parseInt(card.value)
+          return parseInt(card.value);
         }
       }
 
-      let card1 = getCardValue(cards[0])
-      let card2 = getCardValue(cards[1])
-      cardComparison(card1, card2);
-
-      cards.forEach((card, index) => {
+      let card1 = getCardValue(cards[0]);
+      let card2 = getCardValue(cards[1]);
+      let renderedCards = cardComparison(card1, card2);
+      cards.forEach((card) => {
         renderedHtml += `<img src="${card.image}" alt="Card Image">`;
+        
       });
       document.getElementById("card-images").innerHTML = renderedHtml;
+      document.getElementById("card-winner").innerHTML = `<h2> ${renderedCards} </h2>`
+      document.getElementById("cards-remaining").innerHTML = `<h3> Cards Remaining ${cardsRemaming}</h3>`
     });
 }
 
 function cardComparison(card1, card2) {
-  if (card1 > card2){
-    console.log("Card 1 Wins!")
+  if (card1 > card2) {
+    return "Computer Wins!";
   } else if (card1 == card2) {
-    console.log("Its a Tie!")
+    return "Its a Tie!";
   } else {
-    console.log("Card 2 Wins!")
+    return "Player Wins!";
   }
 }
